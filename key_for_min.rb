28033e41bd5f975a3cba4513f13446f3fe6682ee
Angie Kassab <anasab12@gmail.com>
2018-07-05 01:27:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(hash)
   number = nil
   name = nil
   hash.each do |key,value|
-    if value == 1
+    if value <11 || value >0
       name = key
     end
     if hash.length == 0 
